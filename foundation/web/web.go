0cfb4233d800a3ae9870894460ad09f3ba4c71a0
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-10-26 09:12:17 +0330

    feat(foundational/web): added middleware support to framework

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -9,28 +9,32 @@ import (
 )
 
 // HandlerFunc represents a function that handles a http request within our own framework
-type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
+type HandlerFunc func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
 // App is entry point to our app and what configs our context
 type App struct {
 	*http.ServeMux
 	shutdown chan os.Signal
+	mw       []MidFunc
 }
 
 // NewApp creates an App value that handle a set of routes for the application.
-func NewApp(shutdown chan os.Signal) *App {
+func NewApp(shutdown chan os.Signal, mw ...MidFunc) *App {
 	return &App{
 		ServeMux: http.NewServeMux(),
 		shutdown: shutdown,
+		mw:       mw,
 	}
 }
 
 // HandlerFunc sets a handler function for a given HTTP method and path pair
 // to the application server mux.
-func (a *App) HandleFunc(pattern string, handler Handler) {
+func (a *App) HandleFunc(pattern string, handlerFunc HandlerFunc, mw ...MidFunc) {
+	handlerFunc = wrapMiddleware(mw, handlerFunc)
+	handlerFunc = wrapMiddleware(a.mw, handlerFunc)
 
 	h := func(w http.ResponseWriter, r *http.Request) {
-		if err := handler(r.Context(), w, r); err != nil {
+		if err := handlerFunc(r.Context(), w, r); err != nil {
 			// to be error handled proper
 			fmt.Println(err)
 			return
