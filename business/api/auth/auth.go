0334ca12821b4ccd3583ecf7c489a4a041569586
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-11-08 12:18:53 +0330

    feat(business/auth): added base auth package and OPAs

---
+++ business/api/auth/auth.go
@@ -0,0 +1,4 @@
+// Package auth provides authentication and authorization support.
+// Authentication: You are who you say you are.
+// Authorization:  You have permission to do what you are requesting to do.
+package auth
