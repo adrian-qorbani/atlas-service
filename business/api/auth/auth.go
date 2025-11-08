d785345a79cbffa40c0a7c091ae4860bf0f24ac1
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-11-08 12:35:39 +0330

    feat(business/api/auth): added auth creation, structs amd interface.

--- business/api/auth/auth.go
+++ business/api/auth/auth.go
@@ -2,3 +2,65 @@
 // Authentication: You are who you say you are.
 // Authorization:  You have permission to do what you are requesting to do.
 package auth
+
+import (
+	"errors"
+
+	"github.com/adrian-qorbani/atlas-service/foundation/logger"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// ErrForbidden is returned when a auth issue is identified.
+var ErrForbidden = errors.New("attempted action is not allowed")
+
+// Claims represents the authorization claims transmitted via a JWT.
+type Claims struct {
+	jwt.RegisteredClaims
+	Roles []string `json:"roles"`
+}
+
+// HasRole checks if the specified role exists.
+func (c Claims) HasRole(r string) bool {
+	for _, role := range c.Roles {
+		if role == r {
+			return true
+		}
+	}
+	return false
+}
+
+// KeyLookup declares a method set of behavior for looking up
+// private and public keys for JWT use. The return could be a
+// PEM encoded string or a JWS based key.
+type KeyLookup interface {
+	PrivateKey(kid string) (key string, err error)
+	PublicKey(kid string) (key string, err error)
+}
+
+// Config represents information required to initialize auth.
+type Config struct {
+	Log       *logger.Logger
+	KeyLookup KeyLookup
+	Issuer    string
+}
+
+// Auth is used to authenticate clients. It can generate a token for a
+// set of user claims and recreate the claims by parsing the token.
+type Auth struct {
+	keyLookup KeyLookup
+	method    jwt.SigningMethod
+	parser    *jwt.Parser
+	issuer    string
+}
+
+// New creates an Auth to support authentication/authorization.
+func New(cfg Config) (*Auth, error) {
+	a := Auth{
+		keyLookup: cfg.KeyLookup,
+		method:    jwt.GetSigningMethod(jwt.SigningMethodRS256.Name),
+		parser:    jwt.NewParser(jwt.WithValidMethods([]string{jwt.SigningMethodRS256.Name})),
+		issuer:    cfg.Issuer,
+	}
+
+	return &a, nil
+}
