1edd29906155c88c4b1834656883bc554e704f4e
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-11-08 13:03:28 +0330

    feat(business/api/auth): added token gen, authenticate and OPA eval functions, added rules, embeded regos'

--- business/api/auth/auth.go
+++ business/api/auth/auth.go
@@ -4,10 +4,14 @@
 package auth
 
 import (
+	"context"
 	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/adrian-qorbani/atlas-service/foundation/logger"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/open-policy-agent/opa/rego"
 )
 
 // ErrForbidden is returned when a auth issue is identified.
@@ -63,4 +67,110 @@ func New(cfg Config) (*Auth, error) {
 	}
 
 	return &a, nil
+}
+
+// Issuer provides the configured issuer used to authenticate tokens.
+func (a *Auth) Issuer() string {
+	return a.issuer
+}
+
+// GenerateToken generates a signed JWT token string representing the user Claims.
+func (a *Auth) GenerateToken(kid string, claims Claims) (string, error) {
+	token := jwt.NewWithClaims(a.method, claims)
+	token.Header["kid"] = kid
+
+	privateKeyPEM, err := a.keyLookup.PrivateKey(kid)
+	if err != nil {
+		return "", fmt.Errorf("private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(privateKeyPEM))
+	if err != nil {
+		return "", fmt.Errorf("parsing private pem: %w", err)
+	}
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return "", fmt.Errorf("signing token: %w", err)
+	}
+
+	return str, nil
+}
+
+// Authenticate processes the token to validate the sender's token is valid.
+func (a *Auth) Authenticate(ctx context.Context, bearerToken string) (Claims, error) {
+	parts := strings.Split(bearerToken, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return Claims{}, errors.New("expected authorization header format: Bearer <token>")
+	}
+
+	var claims Claims
+	token, _, err := a.parser.ParseUnverified(parts[1], &claims)
+	if err != nil {
+		return Claims{}, fmt.Errorf("error parsing token: %w", err)
+	}
+
+	kidRaw, exists := token.Header["kid"]
+	if !exists {
+		return Claims{}, fmt.Errorf("kid missing from header: %w", err)
+	}
+
+	kid, ok := kidRaw.(string)
+	if !ok {
+		return Claims{}, fmt.Errorf("kid malformed: %w", err)
+	}
+
+	pem, err := a.keyLookup.PublicKey(kid)
+	if err != nil {
+		return Claims{}, fmt.Errorf("failed to fetch public key: %w", err)
+	}
+
+	input := map[string]any{
+		"Key":   pem,
+		"Token": parts[1],
+		"ISS":   a.issuer,
+	}
+
+	if err := a.opaPolicyEvaluation(ctx, regoAuthentication, RuleAuthenticate, input); err != nil {
+		return Claims{}, fmt.Errorf("authentication failed : %w", err)
+	}
+
+	// Check the database for this user to verify they are still enabled. to-do.
+
+	return claims, nil
+}
+
+func (a *Auth) Authorize() error {
+	// to-do
+	return nil
+}
+
+// opaPolicyEvaluation asks opa to evaluate the token against the specified token
+// policy and public key.
+func (a *Auth) opaPolicyEvaluation(ctx context.Context, regoScript string, rule string, input any) error {
+	query := fmt.Sprintf("x = data.%s.%s", opaPackage, rule)
+
+	q, err := rego.New(
+		rego.Query(query),
+		rego.Module("policy.rego", regoScript),
+	).PrepareForEval(ctx)
+	if err != nil {
+		return err
+	}
+
+	results, err := q.Eval(ctx, rego.EvalInput(input))
+	if err != nil {
+		return fmt.Errorf("query: %w", err)
+	}
+
+	if len(results) == 0 {
+		return errors.New("no results")
+	}
+
+	result, ok := results[0].Bindings["x"].(bool)
+	if !ok || !result {
+		return fmt.Errorf("bindings results[%v] ok[%v]", results, ok)
+	}
+
+	return nil
 }
