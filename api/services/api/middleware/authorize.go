4f174bf1a0e0c0526c915268026331262e2b318d
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-11-12 09:28:58 +0330

    fix(api/middleware): added missing rule arg to authorize middleware.

--- api/services/api/middleware/authorize.go
+++ api/services/api/middleware/authorize.go
@@ -10,14 +10,14 @@ import (
 )
 
 // Authorize executes the authorize middleware functionality
-func Authorize(auth *auth.Auth) web.MidFunc {
+func Authorize(auth *auth.Auth, rule string) web.MidFunc {
 
 	m := func(handler web.HandlerFunc) web.HandlerFunc {
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 			hdl := func(ctx context.Context) error {
 				return handler(ctx, w, r)
 			}
-			return middleware.Authorization(ctx, auth, r.Header.Get("authorization"), hdl)
+			return middleware.Authorize(ctx, auth, rule, hdl)
 		}
 		return h
 	}
