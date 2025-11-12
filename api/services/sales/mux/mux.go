88c9260dcf23e30438f3c983665314c4b5964046
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-11-12 09:31:55 +0330

    feat(api/services): added auth middleware to sales. added a testauth to implement the new authenticate and authorize middlewares. WebAPI now takes Auth struct in parameter.

--- api/services/sales/mux/mux.go
+++ api/services/sales/mux/mux.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/adrian-qorbani/atlas-service/api/services/api/middleware"
 	"github.com/adrian-qorbani/atlas-service/api/services/sales/routes/sys/checkapi"
+	"github.com/adrian-qorbani/atlas-service/business/api/auth"
 	"github.com/adrian-qorbani/atlas-service/foundation/logger"
 	"github.com/adrian-qorbani/atlas-service/foundation/web"
 )
 
-func WebAPI(log *logger.Logger, shutdown chan os.Signal) *web.App {
+func WebAPI(log *logger.Logger, auth *auth.Auth, shutdown chan os.Signal) *web.App {
 	m := web.NewApp(shutdown, middleware.Logger(log), middleware.Errors(log), middleware.Metrics(), middleware.Panics())
-	checkapi.Routes(m)
+	checkapi.Routes(m, auth)
 
 	return m
 }
--- api/services/sales/routes/sys/checkapi/routes.go
+++ api/services/sales/routes/sys/checkapi/routes.go
@@ -1,13 +1,20 @@
 package checkapi
 
 import (
+	"github.com/adrian-qorbani/atlas-service/api/services/api/middleware"
+	"github.com/adrian-qorbani/atlas-service/business/api/auth"
 	"github.com/adrian-qorbani/atlas-service/foundation/web"
 )
 
-func Routes(app *web.App) {
+func Routes(app *web.App, a *auth.Auth) {
+
+	authen := middleware.Authenticate(a)
+	athAdminOnly := middleware.Authorize(a, auth.RuleAdminOnly)
 
 	app.HandleFuncNoMiddleware("GET /liveness", liveness)
 	app.HandleFuncNoMiddleware("GET /readiness", readiness)
 	app.HandleFunc("GET /testerror", testError)
 	app.HandleFunc("GET /testpanic", testPanic)
+	app.HandleFunc("GET /testauth", liveness, authen, athAdminOnly)
+
 }
