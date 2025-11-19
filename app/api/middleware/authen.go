c1ec8e4f09b0ac383a302b99c93d3af8e9cf96eb
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-11-19 09:06:01 +0330

    feat(app/auth): added basic authentication.

--- app/api/middleware/authen.go
+++ app/api/middleware/authen.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/adrian-qorbani/atlas-service/app/api/errs"
 	"github.com/adrian-qorbani/atlas-service/business/api/auth"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
 )
 
@@ -19,6 +21,8 @@ func Authorization(ctx context.Context, auth *auth.Auth, authorization string, h
 	switch parts[0] {
 	case "Bearer":
 		ctx, err = processJWT(ctx, auth, authorization)
+	case "Basic":
+		ctx, err = processBasic(ctx)
 	}
 
 	if err != nil {
@@ -48,3 +52,45 @@ func processJWT(ctx context.Context, auth *auth.Auth, token string) (context.Con
 
 	return ctx, nil
 }
+
+// Basic processes basic authentication logic.
+func processBasic(ctx context.Context) (context.Context, error) {
+	claims := auth.Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   "3152420d-c05e-419e-a42a-7e5eb1151cac", //-to-do
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	subjectID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return ctx, errs.Newf(errs.Unauthenticated, "parsing subject: %s", err)
+	}
+
+	ctx = setUserID(ctx, subjectID)
+	ctx = setClaims(ctx, claims)
+
+	return ctx, nil
+}
+
+// func parseBasicAuth(auth string) (string, string, bool) {
+// 	parts := strings.Split(auth, " ")
+// 	if len(parts) != 2 || parts[0] != "Basic" {
+// 		return "", "", false
+// 	}
+
+// 	c, err := base64.StdEncoding.DecodeString(parts[1])
+// 	if err != nil {
+// 		return "", "", false
+// 	}
+
+// 	username, password, ok := strings.Cut(string(c), ":")
+// 	if !ok {
+// 		return "", "", false
+// 	}
+
+// 	return username, password, true
+// }
