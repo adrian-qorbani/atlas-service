dc207b5a6cd2dd095a593e15883115f539d99ceb
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-11-19 09:02:07 +0330

    chore(service/sales): added a desc for webAPI.

--- api/services/sales/mux/mux.go
+++ api/services/sales/mux/mux.go
@@ -10,6 +10,7 @@ import (
 	"github.com/adrian-qorbani/atlas-service/foundation/web"
 )
 
+// WebAPI constructs a http.Handler with all application routes bound.
 func WebAPI(log *logger.Logger, auth *auth.Auth, shutdown chan os.Signal) *web.App {
 	m := web.NewApp(shutdown, middleware.Logger(log), middleware.Errors(log), middleware.Metrics(), middleware.Panics())
 	checkapi.Routes(m, auth)
