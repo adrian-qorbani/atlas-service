5e1d7301ae02b17baa24768bf5bbeb986324bbe4
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-12-03 10:28:14 +0330

    feat(buss/sqldb): added statusCheck.

--- business/sqldb/sqldb.go
+++ business/sqldb/sqldb.go
@@ -2,9 +2,11 @@
 package sqldb
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"net/url"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
@@ -66,4 +68,38 @@ func Open(cfg Config) (*sqlx.DB, error) {
 	db.SetMaxOpenConns(cfg.MaxOpenConns)
 
 	return db, nil
+}
+
+// StatusCheck returns nil if it can successfully talk to the database. It
+// returns a non-nil error otherwise.
+func StatusCheck(ctx context.Context, db *sqlx.DB) error {
+
+	// If the user doesn't give us a deadline set 1 second.
+	if _, ok := ctx.Deadline(); !ok {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, time.Second)
+		defer cancel()
+	}
+
+	var pingError error
+	for attempts := 1; ; attempts++ {
+		pingError = db.Ping()
+		if pingError == nil {
+			break
+		}
+		time.Sleep(time.Duration(attempts) * 100 * time.Millisecond)
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+	}
+
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	// Run a simple query to determine connectivity.
+	// Running this query forces a round trip through the database.
+	const q = `SELECT true`
+	var tmp bool
+	return db.QueryRowContext(ctx, q).Scan(&tmp)
 }
