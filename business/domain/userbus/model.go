c7aed49e252a92618cadb28d1144989ff1d3f31d
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-12-09 10:32:01 +0330

    feat(app/domain/userbus): added userbus user model, role and userdb model.

---
+++ business/domain/userbus/model.go
@@ -0,0 +1,22 @@
+// Package userbus provides business access to user domain.
+package userbus
+
+import (
+	"net/mail"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// User represents information about an individual user.
+type User struct {
+	ID           uuid.UUID
+	Name         string
+	Email        mail.Address
+	Roles        []Role
+	PasswordHash []byte
+	Department   string
+	Enabled      bool
+	DateCreated  time.Time
+	DateUpdated  time.Time
+}
