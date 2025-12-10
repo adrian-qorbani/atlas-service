88df6aae33f67908a9213a0851dd7d750e79d5e3
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-12-10 19:40:29 +0330

    feat(api/http/api/middleware): added AuthorizeUser middleware.

--- api/http/api/middleware/authorize.go
+++ api/http/api/middleware/authorize.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/adrian-qorbani/atlas-service/app/api/authclient"
 	"github.com/adrian-qorbani/atlas-service/app/api/middleware"
+	"github.com/adrian-qorbani/atlas-service/business/domain/userbus"
 	"github.com/adrian-qorbani/atlas-service/foundation/logger"
 	"github.com/adrian-qorbani/atlas-service/foundation/web"
 )
@@ -22,5 +23,25 @@ func Authorize(log *logger.Logger, client *authclient.Client, rule string) web.M
 		}
 		return h
 	}
+	return m
+}
+
+// AuthorizeUser executes the specified role and extracts the specified
+// user from the DB if a user id is specified in the call. Depending on the rule
+// specified, the userid from the claims may be compared with the specified
+// user id.
+func AuthorizeUser(log *logger.Logger, client *authclient.Client, userBus *userbus.Business, rule string) web.MidFunc {
+	m := func(handler web.HandlerFunc) web.HandlerFunc {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			hdl := func(ctx context.Context) error {
+				return handler(ctx, w, r)
+			}
+
+			return middleware.AuthorizeUser(ctx, log, client, userBus, rule, web.Param(r, "user_id"), hdl)
+		}
+
+		return h
+	}
+
 	return m
 }
