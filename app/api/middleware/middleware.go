fe6f73499894a07337f879c39428ff630afd91be
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-12-10 16:38:54 +0330

    feat(app/api/middleware): added setUser amd getUser and AuthorizeUser.

--- app/api/middleware/middleware.go
+++ app/api/middleware/middleware.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"github.com/adrian-qorbani/atlas-service/app/api/auth"
+	"github.com/adrian-qorbani/atlas-service/business/domain/userbus"
 	"github.com/google/uuid"
 )
 
@@ -16,6 +17,7 @@ type ctxKey int
 const (
 	claimKey ctxKey = iota + 1
 	userIDKey
+	userKey
 )
 
 func setClaims(ctx context.Context, claims auth.Claims) context.Context {
@@ -40,6 +42,20 @@ func GetUserID(ctx context.Context) (uuid.UUID, error) {
 	v, ok := ctx.Value(userIDKey).(uuid.UUID)
 	if !ok {
 		return uuid.UUID{}, errors.New("user id not found in context")
+	}
+
+	return v, nil
+}
+
+func setUser(ctx context.Context, usr userbus.User) context.Context {
+	return context.WithValue(ctx, userKey, usr)
+}
+
+// GetUser returns the user from the context.
+func GetUser(ctx context.Context) (userbus.User, error) {
+	v, ok := ctx.Value(userKey).(userbus.User)
+	if !ok {
+		return userbus.User{}, errors.New("user not found in context")
 	}
 
 	return v, nil
