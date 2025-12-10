b01751365f37a970912660128fe641291bfb1039
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2025-12-10 09:47:16 +0330

    feat(bus/userbus): added storer, bus struct, model, filter for userbus

--- business/domain/userbus/userbus.go
+++ business/domain/userbus/userbus.go
@@ -1,2 +1,46 @@
 // Package userbus provides business access to user domain.
 package userbus
+
+import (
+	"context"
+	"errors"
+	"net/mail"
+
+	"github.com/adrian-qorbani/atlas-service/business/api/order"
+	"github.com/adrian-qorbani/atlas-service/foundation/logger"
+	"github.com/google/uuid"
+)
+
+// Set of error variables for CRUD operations.
+var (
+	ErrNotFound              = errors.New("user not found")
+	ErrUniqueEmail           = errors.New("email is not unique")
+	ErrAuthenticationFailure = errors.New("authentication failed")
+)
+
+// Storer interface declares the behavior this package needs to perists and
+// retrieve data.
+type Storer interface {
+	Create(ctx context.Context, usr User) error
+	Update(ctx context.Context, usr User) error
+	Delete(ctx context.Context, usr User) error
+	Query(ctx context.Context, filter QueryFilter, orderBy order.By, pageNumber int, rowsPerPage int) ([]User, error)
+	Count(ctx context.Context, filter QueryFilter) (int, error)
+	QueryByID(ctx context.Context, userID uuid.UUID) (User, error)
+	QueryByIDs(ctx context.Context, userID []uuid.UUID) ([]User, error)
+	QueryByEmail(ctx context.Context, email mail.Address) (User, error)
+}
+
+// Business manages the set of APIs for user access.
+type Business struct {
+	log    *logger.Logger
+	storer Storer
+}
+
+// NewBusiness constructs a user business API for use.
+func NewBusiness(log *logger.Logger, storer Storer) *Business {
+	return &Business{
+		log:    log,
+		storer: storer,
+	}
+}
