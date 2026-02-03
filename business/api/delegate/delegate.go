3051735082a382e4805b794c3fe158d968c9fbfa
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2026-02-03 11:37:14 +0330

    feat(bus/api): added delegate for function calls between dif domains when an import is impossible.

--- business/api/delegate/delegate.go
+++ business/api/delegate/delegate.go
@@ -1,3 +1,19 @@
 // Package delegate provides the ability to make function calls between
 // different domain packages when an import is not possible.
 package delegate
+
+import "github.com/adrian-qorbani/atlas-service/foundation/logger"
+
+// These types are just for documentation so we know what keys go
+// where in the map.
+type (
+	domain string
+	action string
+)
+
+// Delegate manages the set of functions to be called by domain
+// packages when an import is not possible.
+type Delegate struct {
+	log   *logger.Logger
+	funcs map[domain]map[action][]Func
+}
--- business/api/delegate/model.go
+++ business/api/delegate/model.go
@@ -1,1 +1,13 @@
 package delegate
+
+import "context"
+
+// Func represents a function that is registered and called by the system.
+type Func func(context.Context, Data) error
+
+// Data represents an event between domains.
+type Data struct {
+	Domain    string
+	Action    string
+	RawParams []byte
+}
