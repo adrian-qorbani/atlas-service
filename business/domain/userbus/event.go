02d8b0600d26935f95fa30ab7321af87fdd7ac13
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2026-02-04 09:20:17 +0330

    feat(bus): added action updated delegate to user bus.

--- business/domain/userbus/event.go
+++ business/domain/userbus/event.go
@@ -14,11 +14,18 @@ const DomainName = "user"
 // Set of delegate actions.
 const (
 	ActionDeleted = "deleted"
+	ActionUpdated = "updated"
 )
 
 // ActionDeletedParms represents the parameters for the deleted action.
 type ActionDeletedParms struct {
 	UserID uuid.UUID
+}
+
+// ActionUpdatedParms represents the parameters for the updated action.
+type ActionUpdatedParms struct {
+	UserID uuid.UUID
+	UpdateUser
 }
 
 // String returns a string representation of the action parameters.
@@ -45,6 +52,37 @@ func ActionDeletedData(userID uuid.UUID) delegate.Data {
 	return delegate.Data{
 		Domain:    DomainName,
 		Action:    ActionDeleted,
+		RawParams: rawParams,
+	}
+}
+
+// String returns a string representation of the action parameters.
+func (au *ActionUpdatedParms) String() string {
+	return fmt.Sprintf("&EventParamsUpdated{UserID:%v, Enabled:%v}", au.UserID, au.Enabled)
+}
+
+// Marshal returns the event parameters encoded as JSON.
+func (au *ActionUpdatedParms) Marshal() ([]byte, error) {
+	return json.Marshal(au)
+}
+
+// ActionUpdatedData constructs the data for the updated action.
+func ActionUpdatedData(uu UpdateUser, userID uuid.UUID) delegate.Data {
+	params := ActionUpdatedParms{
+		UserID: userID,
+		UpdateUser: UpdateUser{
+			Enabled: uu.Enabled,
+		},
+	}
+
+	rawParams, err := params.Marshal()
+	if err != nil {
+		panic(err)
+	}
+
+	return delegate.Data{
+		Domain:    DomainName,
+		Action:    ActionUpdated,
 		RawParams: rawParams,
 	}
 }
