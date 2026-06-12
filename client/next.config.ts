2443c993cbaa5c8d1d3557049beaeee8be507060
mahdi-developer <mahdi.ghorbani96io@gmail.com>
2026-06-12 10:13:41 +0330

    feat(client): add Dockerfile and env config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  env: {
+    NEXT_PUBLIC_API_URL:  process.env.NEXT_PUBLIC_API_URL  || "http://localhost:3000",
+    NEXT_PUBLIC_AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL || "http://localhost:6000",
+    NEXT_PUBLIC_AUTH_KID: process.env.NEXT_PUBLIC_AUTH_KID || "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1",
+  },
 };
 
 export default nextConfig;
